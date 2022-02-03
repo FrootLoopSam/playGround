291a8b616d2b9191776f12c9bcc62339a3085321
Sam Falcon <samfalc@gmail.com>
2022-02-02 22:05:07 -0500

    install packages; connect to aws and read from ssm; start connecting db

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,9 +7,32 @@ import { Logger } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 
+import { Config, SSM } from 'aws-sdk';
+import * as dotenv from 'dotenv';
+
 import { AppModule } from './app/app.module';
+import {} from '../endpoints.config';
+
+export const env = process.env['FOO'];
 
 async function bootstrap() {
+  dotenv.config();
+
+  // get database connection credentials
+  const ssmProvider = new SSM({ region: 'us-east-1' });
+  const dbURL = ssmProvider.getParameter(
+    {
+      // TODO: check if production is true and swap SSM Param accordingly
+      Name: '/lupe-time/prod/db/url',
+    },
+    (err, res) => {
+      if (err) {
+        throw console.error('Ruh roh, ', err);
+      } else {
+        console.log(res.Parameter.Value);
+      }
+    }
+  );
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
@@ -27,6 +50,9 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
+  Logger.log(`Test: ${process.env.FOO}`);
+  Logger.log(`Node: ${process.env.NODE_ENV}`);
+  Logger.log(`again: ${process.env.FOO}`);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
